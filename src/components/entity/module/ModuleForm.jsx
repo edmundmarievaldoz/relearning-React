ef77079f593fd0d46860dae011f18344c14ed856
Edmund Valdoz <edmundmarievaldoz@gmail.com>
2026-06-10 21:35:52 +0100

    Form tray creation and label, input, and select creation + styling (ModuleForm.jsx and scss)

--- src/components/entity/module/ModuleForm.jsx
+++ src/components/entity/module/ModuleForm.jsx
@@ -1,4 +1,5 @@
 import Action from '../../UI/Actions';
+import Spacer from '../../UI/Spacer.jsx';
 import './ModuleForm.scss';
 
 function ModuleForm ({onCancel}) { //the onCancel prop lives in the module.jsx file
@@ -8,11 +9,57 @@ function ModuleForm ({onCancel}) { //the onCancel prop lives in the module.jsx f
     //view...
     return (
         <div className='moduleForm'>
-            <p>This is the Form</p>
+            <Spacer>
+                <div className='FormTray'>
+
+                <label>
+                    Module Name:
+                    <input type='text' name='ModuleName' />
+                </label>
+
+                <label>
+                    Module Code:
+                    <input type='text' name='ModuleCode' />
+                </label>
+
+                <label>
+                    Module Level:
+                    <select name='ModuleLevel'>
+                        <option value={0} hidden>No Level Selected</option>
+                        {
+                            [3,4,5,6,7].map( (level) => 
+                            <option key={level}>{level}</option> )
+                        }
+                    </select>
+                    {/*this is an array that'll help map out what is inside */}
+                    {/*will allow us to see the numbers in the array */}
+                </label>
+
+                
+                <label>
+                    Module Year:
+                    <input type='text' name='ModuleYear' />
+                </label>
+
+                
+                <label>
+                    Module Leader:
+                    <input type='text' name='ModuleLeader' />
+                </label>
+
+                
+                <label>
+                    Module Image:
+                    <input type='text' name='ModuleImageURL' />
+                </label>
+
+
+            </div>
+
             <Action.Tray>
                 <Action.Cancel showText buttonText='Cancel' onClick={onCancel} /> {/*we call it here */}
             </Action.Tray>
-
+            </Spacer>
         </div>
 
     );
